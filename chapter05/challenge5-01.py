ab4c03d8cb4d6b2ff0b5fc5710dcdd9578e7066d
itc-n26005 <n26005@std.it-college.ac.jp>
2026-06-09 07:57:42 +0000

    第5章例題27まで終了

---
+++ chapter05/challenge5-01.py
@@ -0,0 +1,4 @@
+musician = ["MY FIRST STORY", "Vaudy", "SUPER BEAVER"]
+
+print(musician)
+
---
+++ chapter05/ex5-16.py
@@ -0,0 +1,5 @@
+dys = ("1984", "Brave New World", "Fahrenheit 451")
+dys = (dys[0], "Handmaid's Tale", dys[2])
+
+print(dys)
+
